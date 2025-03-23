107b94a98656828d44010b67f2d53989309a1a6a
nirmal tharuka <tharukanirmal53@gmail.com>
2025-03-23 23:16:41 +0530

    update files

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ import AdvertisementRoutes from "./routes/Advertisement/advertisementRouter.js";
 
 import productRouter from "./routes/Products/productRouter.js";
 
+
 dotenv.config();
 
 const app = express();
